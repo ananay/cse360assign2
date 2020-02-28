e5b56afb01fd06077c77d89399f457d269f2625d
ananay <i@ananayarora.com>
2020-02-27 22:43:01 -0700

    Added append() method with tests

--- src/cse360assign2/SimpleList.java
+++ src/cse360assign2/SimpleList.java
@@ -18,8 +18,8 @@ public class SimpleList {
 	private int length;
 
 	/**
-	 * SimpleList constructor initializes the list of integers and sets the count to
-	 * zero.
+	 * SimpleList constructor initializes the list of integers and sets the 
+	 * count to zero.
 	 */
 	public SimpleList() {
 		list = new int[10];
@@ -28,9 +28,9 @@ public class SimpleList {
 	}
 
 	/**
-	 * Adds the number N to the zeroth index of the array of integers and shifts the
-	 * rest by one so the new element is always at the zeroth index just like in a
-	 * stack.
+	 * Adds the number N to the zeroth index of the array of integers and shifts
+	 * the rest by one so the new element is always at the zeroth index just 
+	 * like in a stack.
 	 * 
 	 * @param n The number to be added to the list
 	 */
@@ -63,8 +63,8 @@ public class SimpleList {
 	}
 
 	/**
-	 * Removes an item from the list. Searches for the parameter value and removes
-	 * the first occurrence of it.
+	 * Removes an item from the list. Searches for the parameter value and 
+	 * removes the first occurrence of it.
 	 * 
 	 * @param n The number that is to be added to the array.
 	 */
@@ -80,7 +80,7 @@ public class SimpleList {
 		} else if (search != -1) {
 			// Make sure the element exists, then create a new array and push
 			// all elements except for that element.
-			int[] newArray = new int[10];
+			int[] newArray = new int[length];
 			for (int i = 0; i < search; i++) {
 				newArray[i] = list[i];
 			}
@@ -90,8 +90,46 @@ public class SimpleList {
 			count--;
 			list = newArray;
 		}
+	
+		// Check if the list has more than 25% empty spots left		
+		if ((float) (length - count) / length > 0.25) {
+			// Shorten the list because the number of empty spots is more than
+			// 25 percent. Push everything to the newly shortened list.
+			int[] newlist = new int[count];
+			for (int iterator = 0; iterator < count; iterator++) {
+				newlist[iterator] = list[iterator];
+			}
+			list = newlist;
+		}
+		
 	}
-
+	
+	/**
+	 * Appends the parameter to the end of the list.
+	 * 
+	 * @param n The number to add
+	 */
+	public void append(int n) {
+		if (length == count) {
+			// The list is full so let's increase it and create the new list of 
+			// greater length
+			length = (int) (length + (length * 0.5));
+			int[] newlist = new int[length];
+					
+			// Move everything to a new list
+			for (int i = 0; i < count; i++) {
+				newlist[i] = list[i];
+			}
+						
+			// Point the list variable to the new list
+			list = newlist;
+		}
+		
+		// Add the parameter to the list
+		list[count] = n;
+		count++;
+	}
+	
 	/**
 	 * Returns the number of elements in the array
 	 * 
--- tests/cse360assign2/SimpleListTest.java
+++ tests/cse360assign2/SimpleListTest.java
@@ -47,4 +47,62 @@ class SimpleListTest {
 		assertEquals("12 11 10 9 8 7 6 5 4 3 2 1", s.toString());
 	}
 	
+	/**
+	 * Test remove with more spots
+	 */
+	@Test
+	void testRemove() {
+		SimpleList s = new SimpleList();
+		s.add(1);
+		s.add(2);
+		s.add(3);
+		s.add(4);
+		s.add(5);
+		s.add(6);
+		s.add(7);
+		s.add(8);
+		s.add(9);
+		s.add(10);
+		s.remove(10);
+		s.remove(9);
+		s.remove(8);
+		assertEquals("7 6 5 4 3 2 1", s.toString());
+	}
+	
+	/**
+	 * Test append
+	 */
+	@Test
+	void testAppend() {
+		SimpleList s = new SimpleList();
+		s.add(1);
+		s.add(2);
+		s.append(3);
+		s.append(4);
+		s.append(5);
+		assertEquals("2 1 3 4 5", s.toString());
+	}
+	
+	/**
+	 * Test append with list full
+	 */
+	@Test
+	void testAppendWithListFull() {
+		SimpleList s = new SimpleList();
+		s.add(1);
+		s.add(2);
+		s.add(3);
+		s.add(4);
+		s.add(5);
+		s.add(6);
+		s.add(7);
+		s.add(8);
+		s.add(9);
+		s.add(10);
+		s.append(11);
+		s.append(12);
+		s.append(13);
+		assertEquals("10 9 8 7 6 5 4 3 2 1 11 12 13", s.toString());
+	}
+	
 }
